fd8bed248de6e78b06473f31d53b81ed6bdb3915
22_Mohammad Raffi Ramadhan <raffi.tkjsmeksa25@gmail.com>
2026-09-05 17:39:53 +0700

    modul matematika v2

--- modul_matematika.py
+++ modul_matematika.py
@@ -1,20 +1,18 @@
-def ganjilgenap(x): 
-    while True: 
-        x = int(input("Masukkan angka :"))
-        if x % 2 == 0:
-            print("genap") 
-        else:
-            print("ganjil")
+def ganjilgenap(x):
+    if x % 2 == 0:
+        print(x, "adalah bilangan genap")
+    else:
+        print(x, "adalah bilangan ganjil")
+
 
 def bilanganprima(n):
-    while True:
-        n = int(input("Masukkan angka :"))    
-        if n <= 1:
+    if n <= 1:
+        print(n, "bukan bilangan prima")
+        return
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             print(n, "bukan bilangan prima")
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                print(n, "bukan bilangan prima")
-                return False
-        print(n, "adalah bilangan prima")
-        return True
+            return
+
+    print(n, "adalah bilangan prima")
